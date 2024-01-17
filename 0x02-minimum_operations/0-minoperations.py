7616323da3bf8a1cbca0d2c58e458c316ee65f6f
THOSSYN <alextosin5@gmail.com>
2024-01-17 12:18:14 -0800

    Updated the task file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""An interview preparation project"""
+
+
+def minOperations(n: int) -> int:
+    """A function that determines number of
+       copy all and paste operations
+    """
+    operations = 0
+    divisor = 2
+
+    if n == 1:
+        return 0
+
+    while n > 1:
+        while n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        divisor += 1
+
+    return operations
