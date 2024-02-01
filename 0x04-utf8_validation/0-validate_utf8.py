9280f1c36da7ed55567ba46d2d5b6d86055abd5d
THOSSYN <alextosin5@gmail.com>
2024-02-01 20:05:16 +0100

    Added requirement files

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script for utf-8 validation"""
+
+
+def validUTF8(data):
+    """A function that validate if an input
+       data is utf-8 compliant
+    """
+    count = 0
+
+    for item in data:
+        if count == 0:
+            if (item >> 5) == '0b110':
+                count = 1
+            elif (item >> 4) == '0b1110':
+                count = 2
+            elif (item >> 3) == '0b11110':
+                count = 3
+            elif (item >> 7) != 0:
+                return False
+        else:
+            if (item >> 6) != '0b10':
+                return False
+            count -= 1
+    return count == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+# A project on utf-8 validation
