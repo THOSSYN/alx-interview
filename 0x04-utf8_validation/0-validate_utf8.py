58b243f6b483064d15cd5ba6f2bcbbb79ba5cd3a
THOSSYN <alextosin5@gmail.com>
2024-02-02 12:40:00 +0100

    Updated 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,19 +9,21 @@ def validUTF8(data: List[int]) -> bool:
        data is utf-8 compliant
     """
     count = 0
-
-    for item in data:
+    for x in data:
         if count == 0:
-            if (item >> 5) == '0b110':
+            if (x >> 7) == 0b0:
+                count = 0
+            elif (x >> 5) == 0b110:
                 count = 1
-            elif (item >> 4) == '0b1110':
+            elif (x >> 4) == 0b1110:
                 count = 2
-            elif (item >> 3) == '0b11110':
+            elif (x >> 3) == 0b11110:
                 count = 3
-            elif (item >> 7) != 0:
+            else:
                 return False
         else:
-            if (item >> 6) != '0b10':
+            if (x >> 6) != 0b10:
                 return False
             count -= 1
+
     return count == 0
