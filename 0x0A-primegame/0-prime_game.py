ea4e5a1a2af7af5c0275364aa274034635311267
THOSSYN <alextosin5@gmail.com>
2024-03-15 00:44:16 +0100

    Addedprime_game.py and README

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,13 +3,20 @@
 
 
 def isPrime(num):
-    """Check if number is prime"""
+    """Check if number is prime using dynamic programming"""
     if num <= 1:
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
             return False
+        i += 6
     return True
+
 
 def isWinner(rounds, choice):
     """Determines the winner of a prime game"""
